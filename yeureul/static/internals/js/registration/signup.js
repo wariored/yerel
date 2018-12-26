115fbcf9795fbe3cfec5af211146de27b02c72b2
mor kaire <kairemor@gmail.com>
2018-12-26 16:36:38 +0000

    jquery signup form contraints

---
+++ yeureul/static/internals/js/registration/signup.js
@@ -0,0 +1,118 @@
+$(function(){
+    $("#username_error_msg").hide() ; 
+    $("email_error_msg").hide() ; 
+    $("password_error_msg").hide() ; 
+    $("password_confirm_error_msg").hide() ; 
+
+    var error_username = false ; 
+    var error_email = false; 
+    var error_password = false ; 
+    var error_password_confirm = false ; 
+
+    $("#form_username").focusout(function(){
+        check_username() ; 
+    }) ; 
+    $("#form_email").focusout(function(){
+        check_email() ; 
+    }) ; 
+    $("#form_password").focusout(function(){
+        check_password() ; 
+    }) ;
+    $("#form_password_confirm").focusout(function(){
+        check_password_confirm() ; 
+    }) ;
+
+    
+}) ; 
+function check_username()
+    {
+        var pattern = /^[a-zA-Z0-9]{5,}$/;
+        var username = $("#form_username").val() ;
+        if(pattern.test(username) && username !== '')
+        {
+            $("#username_error_msg").hide() ; 
+            $("#form_username").css("border-bottom" , "2px solid #34F458") ; 
+        }
+        else
+        {
+            $("#username_error_msg").html("Le nom d'utilisateur doit avoir au moins 5 caracteres") ; 
+            $("#username_error_msg").show() ; 
+            $("#form_username").css("border-bottom" , "2px solid #F90A0A") ;
+            error_username = true ; 
+        }
+    }
+function check_email()
+{
+    var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/ ;
+    var email = $("#form_email").val() ; 
+    if (pattern.test(email) && email !== '')
+    {
+        $("#email_error_msg").hide() ; 
+        $("#form_email").css("border-bottom" , "2px solid #34F458") ;
+    }
+    else
+    {
+        $("#email_error_msg").html("L'adresse email n'est pas valide");
+        $("#email_error_msg").show() ;
+        $("#form_email").css("border-bottom" , "2px solid #F90A0A") ;
+        error_email = true ; 
+    }
+
+}
+
+function check_password()
+{
+    var password_length = $("#form_password").val().length;
+    console.log(password_length) ; 
+    if(password_length<5)
+    {
+        $("#password_error_msg").html("Le mot de passe doit avoir au minimum 5 caracteres") ;
+        $("#password_error_msg").show() ; 
+        $("#form_password").css("border-bottom","2px solid #F90A0A") ;
+        error_password = true ;
+    } 
+    else
+    {
+        $("#password_error_msg").hide() ;
+        $("#form_password").css("border-bottom" , "2px solid #34F458") ;
+    }
+}
+function check_password_confirm()
+{
+    var password = $("#form_password").val() ; 
+    var password_confirm = $("#form_password_confirm").val() ;
+    if (password !=='' && password == password_confirm)
+    {
+        $("#password_confirm_error_msg").hide() ;
+        $("#form_password_confirm").css("border-bottom" , "2px solid #34F458");
+       
+    }
+    else
+    {
+        $("#password_confirm_error_msg").html("Les deux mot de passe ne sont pas identiques")
+        $("#password_confirm_error_msg").show() ; 
+        $("#form_password_confirm").css("border-bottom" , "2px solid #f90A0A");
+        error_password_confirm = true ; 
+    }
+}
+
+$("#registration_form").submit(function(){
+    error_email = false ; 
+    error_password = false ; 
+    error_username = false  ; 
+    error_password_confirm = false ; 
+
+    check_email() ; 
+    check_password() ; 
+    check_username() ; 
+    check_password_confirm() ; 
+
+    if (error_email === false && error_password === false && error_password_confirm === false
+         && error_username === false){ 
+            return true ;   
+         }
+    else{
+        // alert("Veuillez remplir correctement tous les champs avant d'envoyer SVP");
+        return false ; 
+    }     
+}) ; 
