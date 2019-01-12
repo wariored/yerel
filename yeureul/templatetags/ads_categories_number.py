56162fb3eff02244cd6b397d84d6e160abd1108a
mor kaire <kairemor@gmail.com>
2019-01-12 17:28:16 +0000

    The Handling of the footer with django tags

--- yeureul/templatetags/ads_categories_number.py
+++ yeureul/templatetags/ads_categories_number.py
@@ -4,36 +4,15 @@ register = template.Library()
 
 from ads.models import Ad , Category 
 
-# @register.tag()
-# def count_ads_by_categories():
-#     d = dict()
-#     sup = list()
-#     for category in Category.objects.filter(category_type='B'):
-#         if category.sup_category not in sup:
-#             sup.append(category.sup_category)
-#             d[category.sup_category.name]=Ad.objects.filter(subcategory=Category.objects.get(name=category.name)).count()
-#         else:
-#             d[category.sup_category.name]+=Ad.objects.filter(subcategory=Category.objects.get(name=category.name)).count()
-#     return d
-# @register.simple_tag()
-# def count_ads_by_categories():
-#     d = dict()
-#     sup = list()
-#     for category in Category.objects.filter(category_type='B'):
-#         if category.sup_category not in sup:
-#             sup.append(category.sup_category)
-#             d[category.sup_category.name]=Ad.objects.filter(subcategory=Category.objects.get(name=category.name)).count()
-#         else:
-#             d[category.sup_category.name]+=Ad.objects.filter(subcategory=Category.objects.get(name=category.name)).count()
-#     return d.keys()
-@register.inclusion_tag('header_footer/header.html')
-def count_ads_by_categories1():
-    d = dict()
-    sup = list()
+@register.inclusion_tag('header_footer/ads_categories.html')
+def count_ads_by_categories():
+    dict_adsnumber_categories = dict()
+    list_of_sup_categories = list()
+    l = list()
     for category in Category.objects.filter(category_type='B'):
-        if category.sup_category not in sup:
-            sup.append(category.sup_category)
-            d[category.sup_category.name]=Ad.objects.filter(subcategory=Category.objects.get(name=category.name)).count()
+        if category.sup_category not in list_of_sup_categories:
+            list_of_sup_categories.append(category.sup_category)
+            dict_adsnumber_categories[category.sup_category.name]=Ad.objects.filter(subcategory=Category.objects.get(id=category.id)).count()
         else:
-            d[category.sup_category.name]+=Ad.objects.filter(subcategory=Category.objects.get(name=category.name)).count()
-    return {'count_ads_by_categories':d}
+            dict_adsnumber_categories[category.sup_category.name]+=Ad.objects.filter(subcategory=Category.objects.get(id=category.id)).count()
+    return {'count_ads_categories': dict_adsnumber_categories}
