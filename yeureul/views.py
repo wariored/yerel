d5d8e464b3784edd5e1d6975052d07e5d24808c1
wariored <cheikh1005@gmail.com>
2019-02-19 16:25:05 +0000

    format files

--- yeureul/views.py
+++ yeureul/views.py
@@ -23,7 +23,8 @@ from ads.models import AdUser
 
 
 def handler404(request, exception):
- return render(request, 'error404.html', locals())
+    return render(request, 'error404.html', locals())
+
 
 # index of the site
 def index(request):
