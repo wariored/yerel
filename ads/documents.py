d8f4fbac877b8e69138c36290884bc990803254b
wariored <cheikh1005@gmail.com>
2019-03-11 01:16:54 +0000

    trending ads tag

--- ads/documents.py
+++ ads/documents.py
@@ -7,30 +7,30 @@ ads = Index('ads')
 
 @ads.doc_type
 class AdDocument(DocType):
-    ad_user = fields.NestedField(properties={
-        'given_name': fields.StringField(),
-    })
-    subcategory = fields.NestedField(properties={
-        'name': fields.StringField(),
-        'sup_category': fields.NestedField(properties={
-            'name': fields.StringField()
-        })
-    })
-    location = fields.NestedField(properties={
-        'name': fields.StringField(),
-    })
-
-    img = fields.ObjectField(properties={
-        'media': fields.FileField(),
-    })
-
-    random_url = fields.NestedField(properties={
-        'hex': fields.StringField(),
-    })
+    # ad_user = fields.NestedField(properties={
+    #     'given_name': fields.StringField(),
+    # })
+    # subcategory = fields.NestedField(properties={
+    #     'name': fields.StringField(),
+    #     'sup_category': fields.NestedField(properties={
+    #         'name': fields.StringField()
+    #     })
+    # })
+    # location = fields.NestedField(properties={
+    #     'name': fields.StringField(),
+    # })
+    #
+    # img = fields.ObjectField(properties={
+    #     'media': fields.FileField(),
+    # })
+    #
+    # random_url = fields.NestedField(properties={
+    #     'hex': fields.StringField(),
+    # })
 
     class Meta:
         model = Ad
-        related_models = [Category, AdUser, Location, AdFile]
+        # related_models = [Category, AdUser, Location, AdFile]
 
         fields = [
             'id',
@@ -41,17 +41,17 @@ class AdDocument(DocType):
             'condition',
         ]
 
-    def get_instances_from_related(self, tag_instance):
-
-        """If related_models is set, define how to retrieve the instance(s) from the related model.
-        The related_models option should be used with caution because it can lead in the index
-        to the updating of a lot of items.
-        """
-        if isinstance(tag_instance, Category):
-            return tag_instance.subcateg_ads.all()
-        elif isinstance(tag_instance, AdUser):
-            return tag_instance.ads.all()
-        elif isinstance(tag_instance, Location):
-            return tag_instance.loc_ads.all()
-        elif isinstance(tag_instance, AdFile):
-            return tag_instance.ad
+    # def get_instances_from_related(self, tag_instance):
+    #
+    #     """If related_models is set, define how to retrieve the instance(s) from the related model.
+    #     The related_models option should be used with caution because it can lead in the index
+    #     to the updating of a lot of items.
+    #     """
+    #     if isinstance(tag_instance, Category):
+    #         return tag_instance.subcateg_ads.all()
+    #     elif isinstance(tag_instance, AdUser):
+    #         return tag_instance.ads.all()
+    #     elif isinstance(tag_instance, Location):
+    #         return tag_instance.loc_ads.all()
+    #     elif isinstance(tag_instance, AdFile):
+    #         return tag_instance.ad
--- ads/templatetags/global_ads.py
+++ ads/templatetags/global_ads.py
@@ -1,5 +1,6 @@
 from django import template
 from ads.models import Ad, Category, AdFeatured
+from django.utils import timezone
 
 register = template.Library()
 
@@ -26,3 +27,11 @@ def featured(category_id):
     list_ads.sort(key=ads_featured_sorted)
 
     return list_ads
+
+
+@register.simple_tag
+def trending_ads_list():
+    two_weeks_before = timezone.now() - timezone.timedelta(days=14)
+    ads = Ad.objects.filter(creation_date__range=(two_weeks_before, timezone.now()), is_deleted=False,
+                            is_active=True).order_by('-views_number')[:8]
+    return ads
