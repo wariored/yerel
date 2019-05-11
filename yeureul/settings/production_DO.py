eef46061d9a50c8b7080c3264590f44ea70d8fd0
wariored <cheikh1005@gmail.com>
2019-05-11 22:14:29 +0000

    db for prod

--- yeureul/settings/production_DO.py
+++ yeureul/settings/production_DO.py
@@ -5,6 +5,7 @@ BASE_URL = 'https://yerel.co/'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yerel.co', '134.209.19.227']
 
+
 # Application definition
 
 # Database
@@ -13,13 +14,13 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yerel.co', '134.209.19.227']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'USER': get_env_variable('DB_USER'),
+        'PASSWORD': get_env_variable('DB_PASSWORD'),
         'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'PORT': '5432',
+        'NAME': get_env_variable('DB_NAME'),
     }
 }
-DATABASES['default']['USER'] = get_env_variable('DB_USER')
-DATABASES['default']['PASSWORD'] = get_env_variable('DB_PASSWORD')
-DATABASES['default']['NAME'] = get_env_variable('DB_NAME')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
